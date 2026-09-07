8c9b3e03351eb407d88102f4af04f00c828292f0
Mungi Sathish <sathishmungi@gmail.com>
2026-09-07 23:15:17 +0530

    Add password strength checker functionality
    Implement password strength checker based on criteria.

---
+++ Password_strength_checker.py
@@ -0,0 +1,29 @@
+import re
+
+password = input("Enter your password: ")
+
+score = 0
+
+if len(password) >= 8:
+    score += 1
+
+if re.search(r"[A-Z]", password):
+    score += 1
+
+if re.search(r"[a-z]", password):
+    score += 1
+
+if re.search(r"[0-9]", password):
+    score += 1
+
+if re.search(r"[@#$%!&*]", password):
+    score += 1
+
+if score == 5:
+    print("Password Strength: Very Strong")
+elif score >= 3:
+    print("Password Strength: Strong")
+elif score >= 2:
+    print("Password Strength: Medium")
+else:
+    print("Password Strength: Weak")
